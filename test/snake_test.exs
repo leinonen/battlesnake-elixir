c514919fa8ef1cf02b0dc4dac14ae21f5bfef3b2
Peter Leinonen <peter.leinonen@wirelesscar.com>
2017-11-02 15:45:41 +0100

    Added a unit test

--- test/snake_test.exs
+++ test/snake_test.exs
@@ -1,8 +1,17 @@
 defmodule SnakeTest do
-  use ExUnit.Case
+  use ExUnit.Case, async: true
+  use Plug.Test
   doctest Snake
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  @opts Snake.Router.init([])
+
+  test "starts the game" do
+    conn = conn(:post, "/start")
+    conn = Snake.Router.call(conn, @opts)
+
+    assert conn.state == :sent
+    assert conn.status == 200
+    # IO.inspect conn.resp_body
+    # assert conn.resp_body == "world"
   end
 end
