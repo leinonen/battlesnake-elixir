f069fafc656b87075cf68b314447356245018ff7
leinonen <leinonen@paradise.skynet>
2017-11-03 18:50:41 +0100

    decent formatting

--- mix.exs
+++ mix.exs
@@ -2,21 +2,24 @@ defmodule Snake.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :snake,
-     version: "0.1.0",
-     elixir: "~> 1.4",
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
-     deps: deps()]
+    [
+      app: :snake,
+      version: "0.1.0",
+      elixir: "~> 1.4",
+      build_embedded: Mix.env == :prod,
+      start_permanent: Mix.env == :prod,
+      deps: deps()
+    ]
   end
 
   # Configuration for the OTP application
   #
   # Type "mix help compile.app" for more information
   def application do
-    # Specify extra applications you'll use from Erlang/Elixir
-    [extra_applications: [:logger, :cowboy],
-     mod: {Snake, []}]
+    [
+      extra_applications: [:logger, :cowboy],
+      mod: {Snake, []}
+    ]
   end
 
   # Dependencies can be Hex packages:
